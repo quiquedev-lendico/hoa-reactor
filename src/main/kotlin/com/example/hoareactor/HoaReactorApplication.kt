800e1b0aaf2cfc18ee5d5ffc11c8e4c0cf062ee7
Enrique Molina <enrique.molina.ext@lendico.de>
2021-09-14 11:34:37 +0200

    first

---
+++ src/main/kotlin/com/example/hoareactor/HoaReactorApplication.kt
@@ -0,0 +1,11 @@
+package com.example.hoareactor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HoaReactorApplication
+
+fun main(args: Array<String>) {
+    runApplication<HoaReactorApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/hoareactor/HoaReactorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.hoareactor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HoaReactorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
